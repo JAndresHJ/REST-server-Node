2b9c77e01182520f34beada25a308126ace15286
JAndresHJ <jandreshdzj@gmail.com>
2018-09-04 17:11:50 -0500

    Segundo commit

--- server/server.js
+++ server/server.js
@@ -1,42 +1,34 @@
-
-
 require('./config/config');
+
 const express = require('express');
-const bodyParser = require('body-parser')
-const app = express()
+const mongoose = require('mongoose');
 
 
-// app.use son Middlerwares. Cada petición pasa por los middlewares
+const app = express();
+
+const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
- 
+
 // parse application/json
 app.use(bodyParser.json())
 
- 
-app.get('/usuarios', function (req, res) {
-  res.json('get usuario')
-})
 
-// POST Usado para crear nuevos registros
-app.post('/usuarios', function (req, res) {
-    res.json(req.body);
-  })
+app.use(require('./controllers/usuario'));
 
 
-//PUT Usado para actualizar datos
-app.put('/usuarios/:id', function (req, res) {
-    res.json('get usuario');
-  })
- 
-
-// DELETE
-app.delete('/usuarios', function (req, res) {
-    res.json('get usuario');
-  })
 
 
-app.listen(3000, () => {
+mongoose.connect('mongodb://localhost:27017/cafe', { useNewUrlParser: true }, (err, res) => {
+    if (err) throw err;
+    console.log(`Base de datos Online`);
+});
+
+
+app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto: ', 3000);
 });
+
+
+
